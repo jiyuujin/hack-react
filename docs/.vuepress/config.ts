60ae3ff1a8021c1dddd6e07160ef7e49769350d2
jiyuujin <jiyuujinunite@gmail.com>
2021-07-13 08:22:36 +0900

    delete: menu

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -33,7 +33,6 @@ export default defineUserConfig<DefaultThemeOptions>({
         {
           text: `Pokemon アプリ`,
           children: [
-            `/`,
             `/handson/pokemon.md`
           ]
         }
