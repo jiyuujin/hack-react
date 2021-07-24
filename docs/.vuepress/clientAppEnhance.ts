ad6dbe32fc256722467e3b1a0482409516e1053a
jiyuujin <jiyuujinunite@gmail.com>
2021-07-24 09:47:28 +0900

    feat: ad test

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -5,6 +5,23 @@ import { defineClientAppEnhance } from '@vuepress/client'
 export default defineClientAppEnhance(({ app, router, siteData }) => {
   // app.component('MyComponent', Example)
 
+  if (typeof window !== 'undefined') {
+    import('vue-google-adsense')
+      .then((module) => {
+        const ads = module.default
+        app.use(ads.Adsense)
+
+        // 記事内広告
+        app.use(ads.InArticleAdsense)
+
+        // フィード内広告
+        app.use(ads.InFeedAdsense)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   router.beforeEach((to) => {
     // console.log('Before navigation')
   })
