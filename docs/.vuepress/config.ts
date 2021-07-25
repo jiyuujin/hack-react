c9754d166f03215b371e945cf4b84786aa46288f
jiyuujin <jiyuujinunite@gmail.com>
2021-07-26 06:16:29 +0900

    feat: clientId - resolved #7

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,18 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: `Hack React`,
   description: `ミートアップやハンズオンでご紹介のあった資料を中心に記録しています。`,
   head: [
-    //
+    [
+      `script`,
+      {},
+      `(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-7095980629133842", enable_page_level_ads: true });`
+    ],
+    [
+      `script`,
+      {
+        async: true,
+        src: `//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`
+      }
+    ]
   ],
   base: `/`,
   themeConfig: {
